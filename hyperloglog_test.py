26010bc8ffd00b685106aef92c38ce34aba0474c
Juan Lopes <me@juanlopes.net>
2012-08-06 18:36:47 -0300

    hyperloglog compression ok

--- hyperloglog_test.py
+++ hyperloglog_test.py
@@ -3,14 +3,14 @@ from hyperloglog import HyperLogLog
 from random import randint
 
 class TestHyperLogLog(unittest.TestCase):
-    def test_low_range_with_strings(self):
-        self.execute(100, 16, 0.05)
-    
     def test_mid_range_with_strings(self):
-        self.execute(10000, 16, 0.05)
+        self.execute(10000, 10, 0.05)
 
     def test_long_range_with_strings(self):
         self.execute(100000, 10, 0.05)
+
+    def test_low_range_with_strings(self):
+        self.execute(100, 10, 0.05)
 
 
     def execute(self, set_size, m, p):
